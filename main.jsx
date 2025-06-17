bb393db1dd18eb98aafdcc5a8ef7147bde1922f2
James-mcewen <james@silverfoxmedia.co>
2025-06-17 13:12:09 -0500

    fixing paths

--- main.jsx
+++ main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './frontend/App'
-import 'src/frontend/index.css'
+import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
